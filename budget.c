0a49e7dd19d47eb311a77e842294cff985d4f728
newnix <newnix>
2019-03-12 18:14:39 +0000

    Initial Commit
    	* budget.c
    		- Created some prototypes, should just need to pull in both a symmetric and asymmetric crypto algorithm
    		  to be able to complete the necessary includes
    		- Will be expanding on the default table layout and configuration in later commits

---
+++ budget.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2019, Exile Heavy Industries
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the disclaimer
+ * below) provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * * Neither the name of the copyright holder nor the names of its contributors may be used
+ *   to endorse or promote products derived from this software without specific
+ *   prior written permission.
+ * 
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+/* 
+ * This is meant to be a simple number-crunching utility to help with tracking expenses
+ * relative to income. All expenses are able to be given a category to help track if you're 
+ * overspending in certain areas. Since this can hold some valuable information, extra care will be taken 
+ * to help prevent access to the database without proper authentication, though this is not a prioity,
+ * so ensure you take additional methods to secure the database.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sqlite3.h>
+#include <sys/types.h>
+/* man 3 sha512 for information on these functions */
+/* requires -lmd */
+#include <sha512.h>
+
+int readconfig(const char *conffile);
+int init_newdb(const char *dbname, const char *key, const char *pass, const char *categories);
+int mkexpense_category(const char *dbname, const char *category);
+int insert_transaction(const char *dbname, const char *category, int cost);
+void usage(void);
